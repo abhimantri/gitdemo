2ee1066f4c75d4ee1338f52ca8de6fe4a9f404dc
Abhishek Mantripragada <42812595+abhimantri@users.noreply.github.com>
2021-12-31 15:16:47 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,3 +3,27 @@
 apt-get install -y build-essential
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
+nodejs --version
+apt-get install -y ansible
+ansible -version
+apt-get install git -y
+git --version
+apt-get install maven -y
+maven --version
+apt-get install ant -y
+ant --version
+apt-get install azurecli -y
+azurecli --version
+apt-get install -y openssl docker-ce-cli docker-ce containerd* docker-compose nodejs npm openssh terraform groovy python3 composer php
+npm install npm@8.2.0 -g
+openssl --version
+docker --version
+npm --version
+openssh -v
+terraform -v
+groovy --version
+python --version
+php --version
+
+
+
