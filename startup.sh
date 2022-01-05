4fdd344e27f0fc6d1dfb28851ee9424f1b2cdd15
Abhishek Mantripragada <42812595+abhimantri@users.noreply.github.com>
2022-01-05 14:30:16 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,29 +1,4 @@
-## Installing Nodejs 16.X version
-
-apt-get install -y build-essential
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
-nodejs --version
-apt-get install -y ansible
-ansible -version
-apt-get install git -y
-git --version
-apt-get install maven -y
-maven --version
-apt-get install ant -y
-ant --version
-apt-get install azurecli -y
-azurecli --version
-apt-get install -y openssl docker-ce-cli docker-ce containerd* docker-compose nodejs npm openssh terraform groovy python3 composer php
-npm install npm@8.2.0 -g
-openssl --version
-docker --version
-npm --version
-openssh -v
-terraform -v
-groovy --version
-python --version
-php --version
-
+## to get version of a openssh  into a text file
+ssh -V | version.txt
 
 
